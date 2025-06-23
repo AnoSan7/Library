0e5262ff74e0ba4c606095cd6b45d631641f1d7b
AnoSan <anomitra.santra.mat24@itbhu.ac.in>
2025-06-23 12:23:05 +0530

    Create layout

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let books=[];
+let table=document.querySelector("#table");
 function book(bookName,author){
     this.id=crypto.randomUUID()
     this.bookName=bookName;
@@ -9,6 +10,17 @@ function addBook(bookName,author){
     books.push(newBook);
 }
 for(let i=0;i<5;i++){
-    
+    addBook('1','2');
 }
 console.log(books);
+books.forEach((book)=>{
+    let row=document.createElement("tr");
+    for(let elem in book){
+        if(elem!='id'){
+            let cell=document.createElement("td");
+            cell.textContent=book[elem];
+            row.appendChild(cell);
+        }
+    }
+    table.appendChild(row);
+});
